31e0d5cc3e7d2b62bca38024c260df598b401180
Nhut Vinh <nhutvinh14@gmail.com>
2020-03-29 23:05:24 +0700

    :rainbow:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Typography, Card } from "antd";
+import "./App.css";
+
+const { Title, Paragraph, Text } = Typography;
+const { Meta } = Card;
 
 function App() {
   return (
     <div className="App">
-    <div>VVV</div>
+      <div className="_header">
+        <Title level={4}>Truyện ngân sắc ... ngắn :rainbow:</Title>
+        <Text >Một ngày buồn nhó lại ngày xưa, biết rằng giấc mơ sẽ không bao giờ trở thành sự thật nhưng vẫn chờ cơ hội nắm chặt bàn tay ai.</Text>
+     
+        {/* <Card
+          hoverable
+          style={{ width: 240 }}
+          cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        >
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card> */}
+      </div>
     </div>
   );
 }
